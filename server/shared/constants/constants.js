30264d5f1b20b9a6da05b06fe93c0069cbe44d09
Alex <alextjd1@gmail.com>
2020-02-18 22:51:10 +0100

    Add a constants file
    This constants file defines each single
    property as an immutable object for its
    usage amongst the application. This way,
    despite any part of the code tries to update
    an ennumerable property of the object, it
    will stay the same: it will stay constant.

---
+++ server/shared/constants/constants.js
@@ -0,0 +1,21 @@
+function define(name, value) {
+  Object.defineProperty(exports, name, {
+    value: value,
+    enumerable: true
+  });
+}
+
+define("monthNames", [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+]);
