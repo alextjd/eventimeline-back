5d622adba5d255dff3f0253396f70461d99e02b3
Alex <alextjd1@gmail.com>
2020-02-18 23:52:00 +0100

    Fix error in typing object array

--- server/models/day.js
+++ server/models/day.js
@@ -4,39 +4,45 @@ var schema = new mongoose.Schema({
   date: String,
   url: String,
   data: {
-    Events: {
-      year: String,
-      html: String,
-      text: String,
-      links: [
-        {
-          title: String,
-          link: String
-        }
-      ]
-    },
-    Births: {
-      year: String,
-      html: String,
-      text: String,
-      links: [
-        {
-          title: String,
-          link: String
-        }
-      ]
-    },
-    Deaths: {
-      year: String,
-      html: String,
-      text: String,
-      links: [
-        {
-          title: String,
-          link: String
-        }
-      ]
-    }
+    Events: [
+      {
+        year: String,
+        html: String,
+        text: String,
+        links: [
+          {
+            title: String,
+            link: String
+          }
+        ]
+      }
+    ],
+    Births: [
+      {
+        year: String,
+        html: String,
+        text: String,
+        links: [
+          {
+            title: String,
+            link: String
+          }
+        ]
+      }
+    ],
+    Deaths: [
+      {
+        year: String,
+        html: String,
+        text: String,
+        links: [
+          {
+            title: String,
+            link: String
+          }
+        ]
+      }
+    ]
   }
 });
 
