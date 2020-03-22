f78d83f6f17a09ba3025bae1cf2e54a86c77ded2
Alex <alextjd1@gmail.com>
2020-03-22 19:40:35 +0100

    Use the default express cors package

--- server.js
+++ server.js
@@ -15,14 +15,7 @@ const app = express();
 require("./server/models/day");
 
 // CORS setup
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+app.use(cors());
 
 // Routes
 const indexRouter = require("./server/routes/index");
